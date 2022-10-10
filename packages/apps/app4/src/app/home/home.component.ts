dae761e080e2638ec811cb008a52006711f05d23
Jhon Moofarrif <jhon.moofarrif@segurosbolivar.com>
2022-10-10 18:13:31 -0500

    router angular and vite rename

--- packages/apps/app4/src/app/home/home.component.ts
+++ packages/apps/app4/src/app/home/home.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  //TODO investigar formas de redirigir sin routerlink
   constructor() {}
 
   ngOnInit(): void {}
